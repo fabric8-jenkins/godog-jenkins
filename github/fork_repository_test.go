e355e9bddd9db364fbc145347aa4030bf8769dc6
James Strachan <james.strachan@gmail.com>
2017-10-11 20:44:26 +0100

    polish test case with better assertions

--- github/fork_repository_test.go
+++ github/fork_repository_test.go
@@ -1,22 +1,40 @@
 package github
 
 import (
-	"github.com/DATA-DOG/godog"
 	"fmt"
+
+	"github.com/DATA-DOG/godog"
 )
 
-func thereIsNoForkOf(repo string) error {
+type forkFeature struct {
+	GitCommander *GitCommander
+
+	UpstreamDir string
+	ForkDir     string
+}
+
+func (f *forkFeature) thereIsNoForkOf(repo string) error {
 	return nil
 }
 
-func iForkTheGitHubOrganisationToUser(originalRepo string, newUser string) error {
-	userRepo, err := ParseUserRepositoryName(originalRepo)
+func (f *forkFeature) iForkTheGitHubOrganisationToUser(originalRepoName string, newUser string) error {
+	userRepo, err := ParseUserRepositoryName(originalRepoName)
 	if err != nil {
-		return err;
+		return err
 	}
 	client, err := CreateGitHubClient()
 	if err != nil {
-		return err;
+		return err
+	}
+	gitcmder := f.GitCommander
+	err = gitcmder.DeleteWorkDir()
+	if err != nil {
+		return err
+	}
+
+	upstreamRepo, err := GetRepository(client, userRepo.Organisation, userRepo.Repository)
+	if err != nil {
+		return err
 	}
 
 	// now lets fork it
@@ -24,21 +42,58 @@ func iForkTheGitHubOrganisationToUser(originalRepo string, newUser string) error
 	if err != nil {
 		return err
 	}
-	gitcmder := CreateGitCommander()
 	dir, err := gitcmder.Clone(repo)
 	if err == nil {
 		fmt.Printf("Cloned to directory: %s\n", dir)
 	}
-	return err
+	f.ForkDir = dir
+
+	upstreamCloneURL, err := GetCloneURL(upstreamRepo, true)
+	if err != nil {
+		return err
+	}
+
+	upstreamDir, err := gitcmder.CloneFromURL(upstreamRepo, upstreamCloneURL)
+	if err != nil {
+		return err
+	}
+	f.UpstreamDir = upstreamDir
+
+	err = gitcmder.ResetMasterFromUpstream(dir, upstreamCloneURL)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func thereShouldBeAForkWhichHasTheSameLastCommitAs(forkedRepo, originalRepo string) error {
-	return godog.ErrPending
+func (f *forkFeature) thereShouldBeAForkWhichHasTheSameLastCommitAs(forkedRepo, originalRepo string) error {
+	gitcmder := f.GitCommander
+	upstreamSha, err := gitcmder.GetLastCommitSha(f.UpstreamDir)
+	if err != nil {
+		return err
+	}
+	forkSha, err := gitcmder.GetLastCommitSha(f.ForkDir)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("upstream last commit is %s\n", upstreamSha)
+	fmt.Printf("fork last commit is %s\n", forkSha)
+
+	errors := CreateErrorSlice()
+	assert := CreateAssert(errors)
+
+	msg := fmt.Sprintf("The git sha on the fork should be the same as the upstream repository in dir %s and %s", f.ForkDir, f.UpstreamDir)
+	assert.Equal(upstreamSha, forkSha, msg)
+	return errors.Error()
 }
 
 func FeatureContext(s *godog.Suite) {
-	s.Step(`^there is no fork of "([^"]*)"$`, thereIsNoForkOf)
-	s.Step(`^I fork the "([^"]*)" GitHub organisation to user "([^"]*)"$`, iForkTheGitHubOrganisationToUser)
-	s.Step(`^there should be a "([^"]*)" fork which has the same last commit as "([^"]*)"$`, thereShouldBeAForkWhichHasTheSameLastCommitAs)
-}
+	f := &forkFeature{
+		GitCommander: CreateGitCommander(),
+	}
 
+	s.Step(`^there is no fork of "([^"]*)"$`, f.thereIsNoForkOf)
+	s.Step(`^I fork the "([^"]*)" GitHub organisation to user "([^"]*)"$`, f.iForkTheGitHubOrganisationToUser)
+	s.Step(`^there should be a "([^"]*)" fork which has the same last commit as "([^"]*)"$`, f.thereShouldBeAForkWhichHasTheSameLastCommitAs)
+}
